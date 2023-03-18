8ea035c35a58e18716f82068e1810fe66ef9219d
Thabo Lebelo-Mvelase <thabolebelo@icloud.com>
2023-03-18 18:46:48 +0200

    First commit

---
+++ test/wso2-micro-integrator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Wso2MicroIntegrator from '../lib/wso2-micro-integrator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wso2-micro-integrator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Wso2MicroIntegrator.Wso2MicroIntegratorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
